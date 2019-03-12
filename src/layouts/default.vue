a4b0d04caad51f2a9dee16161ef59c3b48d0fabb
test <test@qq.com>
2019-03-12 18:22:13 +0800

    source code push

---
+++ src/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container style="background: #f0f2f5;">
+    <el-menu
+      :collapse="collapse"
+      class="aside-menu"
+      :default-active="$route.path"
+      router
+      background-color="#001529"
+      text-color="hsla(0,0%,100%,.67)"
+    >
+      <div class="logo">
+        <nuxt-link to="/">
+          <img class="logo-img" :src="$store.state.meta.logo" alt="logo" />
+          <h1 class="logo-text">{{ $store.state.meta.appName }}</h1>
+        </nuxt-link>
+      </div>
+      <menu-item :menuList="menuList"></menu-item>
+    </el-menu>
+
+    <el-container>
+      <el-header>
+        <el-row type
