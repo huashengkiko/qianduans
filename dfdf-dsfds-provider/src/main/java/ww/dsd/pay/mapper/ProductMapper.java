776f7934acb855e9945c471ed183da8e867686f1
test <test@qq.com>
2019-03-13 19:11:17 +0800

    source code push

---
+++ dfdf-dsfds-provider/src/main/java/ww/dsd/pay/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package ww.dsd.pay.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import ww.dsd.pay.domain.eo.Product;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Created by donh on 2018/7/24.
+ */
+public interface ProductMapper extends BaseMapper<Product> {
+
+    List<Product> selectPageVo(@Param("price") Integer price);
+}
